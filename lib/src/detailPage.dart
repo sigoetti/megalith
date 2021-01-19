76bc6ee69f343316a00a04eee707cf773d479f01
Silvia Götti <dev@silviagoetti.ch>
2021-01-19 17:47:06 +0100

    added biblio + scrolling

--- lib/src/detailPage.dart
+++ lib/src/detailPage.dart
@@ -20,44 +20,65 @@ class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Material(
-        child: Column(children: <Widget>[
-      Image.network(
-          'https://upload.wikimedia.org/wikipedia/commons/8/85/Merrymaidens5.jpg'),
-      Padding(
-        padding: EdgeInsets.all(30.0),
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Text(
-                  location,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30.0,
+        child: SingleChildScrollView (
+          child:   Column(children: <Widget>[
+            Image.network(
+                'https://upload.wikimedia.org/wikipedia/commons/8/85/Merrymaidens5.jpg'),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        location,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                    ],
                   ),
-                  textAlign: TextAlign.left,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Expanded(
-                  child: Text('N: '+ gps.latitude.toString() +' / O: '+ gps.longitude.toString()),
-                ),
-                Icon(Icons.location_on),
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 20.0, 0, 20.0),
-              child: Text(
-                description,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Expanded(
+                        child: Text('N: '+ gps.latitude.toString() +' / O: '+ gps.longitude.toString()),
+                      ),
+                      Icon(Icons.location_on),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 20.0, 0, 20.0),
+                    child: Text(
+                      description,
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Row( children: [
+                          Text('Literatur',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),),
+                        ], ),
+                        Row( children: [
+                          Text(biblio)
+                        ],)
+                      ],
+                    ),
+                  )
+
+                ],
               ),
             ),
+          ]),
+        )
 
-          ],
-        ),
-      ),
-    ]));
+
+      );
   }
 }
